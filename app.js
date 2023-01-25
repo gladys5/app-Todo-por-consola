96f9af024217093d77a40489df4a9e55cce882c6
gladys5 <yumanji@live.com>
2023-01-25 12:35:14 -0700

    agregando loop para evitar se salga de la app

--- app.js
+++ app.js
@@ -1,10 +1,21 @@
 require("colors")
-const { mostrarMenu } = require("./helpers/mensajes")
+const { mostrarMenu,pausa } = require("./helpers/mensajes")
 
 console.clear
 
 const main = async () => {
+  let opt =''
+  //espera una opcion desde resolve que guardamos en variable opt
+  do{
+opt = await mostrarMenu()
+console.log({opt})
+//pasamamos la pausa para que sea tomada inmediatamente despues de el primer resolve con esto evitamos se limpie la consola  antes de resivir el enter asi provocamos un loop infinito que evita que se salga de la app 
+await pausa()
+  }
+  //ejecutando la opcion 
+  while(opt !=='0')
   console.log("hola")
-  mostrarMenu()
+  close()
+ 
 }
 main()
